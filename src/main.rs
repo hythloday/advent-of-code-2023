2aca18d62038c74ac3e1e3930e7358cb05991160
RC James Harlow <git@james.harlow.name>
2023-12-14 17:57:57 +0000

    boilerplate for day solutions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use std::{error::Error, fs, cmp::Ordering};
+use std::fmt::Debug;
+use std::fmt;
+use itertools::Itertools;
+
+type T = Vec<()>;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = read_input(fs::read_to_string("input/dayX.txt")?)?;
+    println!("{}", solution1(&input)?);
+    println!("{}", solution2(&input)?);
+    Ok(())
+}
+
+fn read_input(s: String) -> Result<T, Box<dyn Error>> {
+    s.trim().lines().map(|line| {
+        todo!()
+    }).collect()
+}
+
+fn solution1(input: &T) -> Result<u64, Box<dyn Error>> {
+    todo!()
+}
+
+fn solution2(input: &T) -> Result<u64, Box<dyn Error>> {
+    todo!()
+}
+
+#[cfg(test)]
+mod test {
+    use crate::*;
+
+    #[test]
+    fn test() -> Result<(), Box<dyn Error>> {
+        Ok(())
+    }
+
+
 }
